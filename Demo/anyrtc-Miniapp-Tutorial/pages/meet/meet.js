7a9104847a4c460d63861a3fdd380ea90fb38a69
chenfaduo <chenfaduo@dync.cc>
2021-10-13 10:50:05 +0800

    删除不必要的代码, 测试日志功能

--- Demo/anyrtc-Miniapp-Tutorial/pages/meet/meet.js
+++ Demo/anyrtc-Miniapp-Tutorial/pages/meet/meet.js
@@ -1,6 +1,7 @@
 import config from "../../utils/config";
 import ArRTC from "../../utils/ArRTCMiniapp@latest";
 var log = require('../../utils/log.js');
+
 Page({
   data: {
     enable: true,
@@ -13,45 +14,16 @@ Page({
     whiteness: 0,
     publishUrl: "",
     operationUser: [],
-    enableAudio: true,
-    timer: null,
+    enableAudio: true
   },
-
   onLoad(options) {
-    log.info(`${this.data.userId} 正在发布视频流, 房间号：${options.roomId}`);
+    wx.setNavigationBarTitle({ title: `房间号： ${options.roomId}` });
     wx.showLoading({
       title: '正在发布视频流，请稍后',
       mask: true,
     });
-    const timer = setTimeout(() => {
-      wx.hideLoading();
-      log.error(`${this.data.userId} 视频流发布失败, 房间号：${options.roomId}`);
-      wx.showToast({
-        icon: 'none',
-        title: '视频流发布失败，请稍后重试',
-        mask: true,
-      });
-      setTimeout(() => {
-        wx.hideToast();
-        const { data: { client }, clearData } = this;
-        wx.navigateBack({ delta: 1 });
-        client.destroy(() => {
-          clearData();
-        }, (e) => {
-          log.error(`${this.data.userId} 销毁客户端对象失败, 房间号：${options.roomId}，错误信息：${e}`);
-          wx.showToast({
-            title: '销毁客户端对象失败',
-            icon: 'none',
-            mask: true,
-          });
-        });
-      }, 3000);
-    }, 15000);
-    this.setData({ timer });
-    wx.setNavigationBarTitle({ title: `房间号： ${options.roomId}` });
-
     let roomId = "" + options.roomId;
-    this.onScreen();
+    this.onScreenChange(true);
     this.setData({ roomId }, () => {
       this.init();
     });
@@ -81,10 +53,7 @@ Page({
       const { uid } = data;
       // 订阅远端媒体流
       client.subscribe(uid, (url) => {
-        this.data.operationUser.push({
-          uid,
-          pullUrl: url,
-        });
+        this.data.operationUser.push({ uid, pullUrl: url });
         this.setData({ operationUser: this.data.operationUser }, () => {
           log.info(`${this.data.userId} 订阅远端媒体流成功, 房间号：${this.data.roomId}`);
         });
@@ -110,41 +79,25 @@ Page({
       this.setData({ operationUser: this.data.operationUser });
     });
 
-    // 通知应用程序已更新 Url 地址。 该回调中会包含远端用户的 ID 和更新后的拉流地址
-    client.on('update-url', (data) => {
-      const { uid, url } = data;
-      this.data.operationUser.forEach((item) => {
-        if (item.uid == uid) {
-          log.info(`${uid} 远端用户已更新 Url 地址, 房间号：${options.roomId}, 更改后的 Url 地址：${url}`);
-          item.pullUrl = url;
-        }
-      });
-      this.setData({ operationUser: this.data.operationUser });
-    });
-
     this.setData({ client }, () => {
       this.addChannel();
     });
   },
 
-  bindstatechange(code) {
-    console.log(code.detail.message, '-------------', code.detail.code)
-  },
+  
 
   // 加入频道
   addChannel() {
-    const { client, userId, roomId, timer } = this.data;
+    const { client, userId, roomId } = this.data;
     client.join(undefined, roomId, userId, () => {
-      log.info(`${this.data.userId} 加入房间成功, 房间号：${this.data.roomId}`);
+      log.info(`${userId} 加入房间成功, 房间号：${roomId}`);
       // 发布本地视频流
+      log.info(`${userId} 正在发布视频流, 房间号：${roomId}`);
       client.publish((url) => {
-        clearTimeout(timer);
-        wx.hideLoading();
-        log.info(`${this.data.userId} 视频流发布成功, 房间号：${this.data.roomId}`);
+        log.info(`${userId} 视频流发布成功, 房间号：${roomId}`);
         this.setData({ publishUrl: url });
       }, (e) => {
-        wx.hideLoading();
-        log.error(`${this.data.userId} 视频流发布失败, 房间号：${this.data.roomId}，错误信息：${e}`);
+        log.error(`${userId} 视频流发布失败, 房间号：${roomId}，错误信息：${e}`);
         wx.showToast({
           icon: 'none',
           title: '视频流发布失败，请稍后重试',
@@ -154,20 +107,19 @@ Page({
         setTimeout(() => {
           wx.hideToast();
           this.setData({ client: null });
-          wx.navigateBack({
-            delta: 1,
-          });
+          wx.navigateBack({ delta: 1 });
         }, 3000);
       });
+      wx.hideLoading();
     }, (e) => {
-      log.error(`${this.data.userId} 加入房间失败, 房间号：${options.roomId}，错误信息：${e}`);
+      log.error(`${userId} 加入房间失败, 房间号：${roomId}，错误信息：${e}`);
     });
   },
 
   // 切换摄像头
   switchCamera() {
     wx.createLivePusherContext().switchCamera();
-    log.info(`${this.data.userId} 切换摄像头, 房间号：${options.roomId}`);
+    log.info(`${this.data.userId} 切换了摄像头, 房间号：${options.roomId}`);
   },
 
   // 切换美颜
@@ -177,11 +129,7 @@ Page({
       beauty: this.data.beauty === 0 ? 9 : 0,
       whiteness: this.data.whiteness === 0 ? 9 : 0,
     }, () => {
-      if (this.data.beauty === 9) {
-        log.info(`${this.data.userId} 开启了美颜, 房间号：${this.data.roomId}`);
-      } else {
-        log.info(`${this.data.userId} 关闭了美颜, 房间号：${this.data.roomId}`);
-      }
+      log.info(`${this.data.userId} ${this.data.beauty === 9? '开启' : '关闭'}了美颜, 房间号：${this.data.roomId}`);
     });
   },
 
@@ -189,108 +137,41 @@ Page({
   switchAudio() {
     const { client, enableAudio } = this.data;
     client[enableAudio ? 'muteLocal' : 'unmuteLocal']('audio', () => {
-      this.setData({
-        enableAudio: !enableAudio,
-      }, () => {
-        if (this.data.enableAudio) {
-          log.info(`${this.data.userId} 开启了声音, 房间号：${this.data.roomId}`);
-        } else {
-          log.info(`${this.data.userId} 关闭了声音, 房间号：${this.data.roomId}`);
-        }
+      this.setData({ enableAudio: !enableAudio }, () => {
+        log.info(`${this.data.userId} ${this.data.enableAudio? '开启' : '关闭'}了声音, 房间号：${this.data.roomId}`);
       });
     });
   },
 
   // 切换 debug
   switchDebug() {
-    this.setData({
-      debug: !this.data.debug
-    }, () => {
-      if (this.data.debug) {
-        log.info(`${this.data.userId} 开启debug, 房间号：${this.data.roomId}`);
-      } else {
-        log.info(`${this.data.userId} 关闭debug, 房间号：${this.data.roomId}`);
-      }
+    this.setData({ debug: !this.data.debug }, () => {
+      log.info(`${this.data.userId} ${this.data.debug? '开启' : '关闭'}了debug, 房间号：${this.data.roomId}`);
     });
   },
 
-  // 离开房间
-  leaveRoom() {
-    const { data: { client, roomId, userId }, clearData } = this;
-    wx.showModal({
-      title: "提示",
-      content: "确认退出会议？",
-      success(res) {
-        if (res.confirm) {
-          // 销毁客户端对象
-          client.destroy(() => {
-            clearData();
-            log.info(`${userId} 销毁客户端对象成功, 房间号：${roomId}`);
-            wx.navigateBack({ delta: 1 });
-          }, (e) => {
-            log.error(`${userId} 销毁客户端对象失败, 房间号：${roomId}，错误信息：${e}`);
-            wx.showToast({
-              title: '销毁客户端对象失败',
-              icon: 'none',
-              mask: true,
-            });
-          });
-        }
-      }
-    });
-  },
-
-  // 重置 data
-  clearData() {
-    this.setData({
-      enable: true,
-      client: null,
-      roomId: "",
-      userId: "weixin" + parseInt(Math.random() * 10000),
-      debug: false,
-      beautyStyle: 'nature',
-      beauty: 0,
-      whiteness: 0,
-      publishUrl: "",
-      operationUser: [],
-      enableAudio: true,
-      timer: null,
-    });
-  },
-
-  // 保持屏幕常亮
-  onScreen() {
-    wx.setKeepScreenOn({
-      keepScreenOn: true
-    });
-  },
-
-  // 关闭屏幕常亮
-  offScreen() {
-    wx.setKeepScreenOn({
-      keepScreenOn: false
-    });
+  // 设置屏幕是否常亮
+  onScreenChange(bool) {
+    wx.setKeepScreenOn({ keepScreenOn: bool });
   },
 
   // 生命周期函数--监听页面隐藏
   onShareAppMessage: function (res) {
-    // return custom share data when user share.
     return {
       title: "快来参加我们的视频会议吧~",
       path: "/pages/meet/meet?roomId=" + this.data.roomId
     };
   },
 
-  // 生命周期函数--监听页面卸载
-  onUnload() {
-    this.offScreen();
-    const { data: { client }, clearData } = this;
+  destruction() {
+    this.onScreenChange(false);
+    const { client, userId, roomId } = this.data;
     if (client == null) return
     client.destroy(() => {
-      clearData();
-      log.info(`${this.data.userId} 销毁客户端对象成功, 房间号：${this.data.roomId}`);
+      log.info(`${userId} 销毁客户端对象成功, 房间号：${roomId}`);
+      wx.navigateBack({ delta: 1 });
     }, (e) => {
-      log.error(`${this.data.userId} 销毁客户端对象失败, 房间号：${this.data.roomId}，错误信息：${e}`);
+      log.error(`${userId} 销毁客户端对象失败, 房间号：${roomId}，错误信息：${e}`);
       wx.showToast({
         title: '销毁客户端对象失败',
         icon: 'none',
@@ -298,21 +179,29 @@ Page({
       });
     });
   },
+
+  // 离开房间
+  leaveRoom() {
+    const { destruction } = this;
+    wx.showModal({
+      title: "提示",
+      content: "确认退出会议？",
+      success(res) {
+        res.confirm && destruction();
+      }
+    });
+  },
+
+  bindstatechange(code) {
+    console.log(code.detail.message, '-------------', code.detail.code)
+  },
+
+  // 生命周期函数--监听页面卸载
+  onUnload() {
+    this.destruction();
+  },
   
   onHide() {
-    this.offScreen();
-    const { data: { client }, clearData } = this;
-    if (client == null) return
-    client.destroy(() => {
-      clearData();
-      log.info(`${this.data.userId} 销毁客户端对象成功, 房间号：${this.data.roomId}`);
-    }, (e) => {
-      log.error(`${this.data.userId} 销毁客户端对象失败, 房间号：${this.data.roomId}，错误信息：${e}`);
-      wx.showToast({
-        title: '销毁客户端对象失败',
-        icon: 'none',
-        mask: true,
-      });
-    });
+    this.destruction();
   }
 });
