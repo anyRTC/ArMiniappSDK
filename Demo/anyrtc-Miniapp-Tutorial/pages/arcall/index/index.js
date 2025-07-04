ce9fdfd49364eaa3f30c1615041252e63072f803
chudong <604748948@qq.com>
2025-07-04 17:57:15 +0800

    refactor: RTM 登录移动至arcall页面；代码格式化

--- Demo/anyrtc-Miniapp-Tutorial/pages/arcall/index/index.js
+++ Demo/anyrtc-Miniapp-Tutorial/pages/arcall/index/index.js
@@ -1,58 +1,66 @@
-// index.js
-// 获取应用实例
-import {
-    Store,RTM
-} from "../../../utils/arcall/index";
+import { Store, RTM } from "../../../utils/arcall/index";
+
 Page({
-    data: {
-        motto: '',
-    },
-    // 初始化
-    onLoad() {
-        Store.State = 0;
-        this.setData({
-            motto: Store.userId
-        })
-
-    },
-    // 返回
-    goBack() {
-        wx.showLoading({
-            title: '跳转中',
-        })
-        wx.reLaunch({
-            url: '../../index/index',
-            success: async () => {
-                // 销毁相关实例
-                await RTM.DestroyRTM()
-                wx.hideLoading();
-            }
-        })
-    },
-    // 跳转至p2p
-    bindViewTapP2p() {
-        if (this.data.motto) {
-            wx.showLoading({
-                title: '跳转中',
-            })
-            wx.navigateTo({
-                url: '../p2p/p2p',
-                success: () => {
-                    wx.hideLoading()
-                }
-            })
+  data: {
+    motto: '',
+  },
+  // 初始化
+  async onLoad() {
+    // 获取当前网络状态
+    wx.getNetworkType({
+      success: async (res) => {
+        console.log("获取当前网络状态", res);
+        if (res.networkType == 'none') {
+          wx.showLoading({
+            title: '没有网络',
+            mask: true
+          })
         } else {
-            wx.showToast({
-                icon: 'none',
-                title: 'RTM 登录中',
-                mask: true,
-                duration: 2000
-            })
+          // anyRTM 初始化
+          await RTM.InItRtm();
+          Store.State = 0;
+          this.setData({
+            motto: Store.userId
+          })
         }
+      }
+    });
+  },
+  // 返回
+  goBack() {
+    wx.showLoading({
+      title: '跳转中',
+    })
+    wx.reLaunch({
+      url: '../../index/index',
+      success: async () => {
+        // 销毁相关实例
+        await RTM.DestroyRTM()
+        wx.hideLoading();
+      }
+    })
+  },
+  // 跳转至p2p
+  bindViewTapP2p() {
+    if (this.data.motto) {
+      wx.showLoading({
+        title: '跳转中',
+      })
+      wx.navigateTo({
+        url: '../p2p/p2p',
+        success: () => {
+          wx.hideLoading()
+        }
+      })
+    } else {
+      wx.showToast({
+        icon: 'none',
+        title: 'RTM 登录中',
+        mask: true,
+        duration: 2000
+      })
+    }
 
-    },
-    // 跳转至多人
-
-
-
+  },
+  // 跳转至多人
 })
--- Demo/anyrtc-Miniapp-Tutorial/pages/index/index.js
+++ Demo/anyrtc-Miniapp-Tutorial/pages/index/index.js
@@ -1,30 +1,10 @@
-import { RTM } from "../../utils/arcall/index"
-
 Page({
   switchMode(e) {
     const type = e.currentTarget.dataset.type;
     if (type == 1) {
-
-      // 获取当前网络状态
-      wx.getNetworkType({
-        async success(res) {
-          console.log("获取当前网络状态", res);
-          if (res.networkType == 'none') {
-            wx.showLoading({
-              title: '没有网络',
-              mask: true
-            })
-          } else {
-            // anyRTM 初始化
-            await RTM.InItRtm();
-            wx.navigateTo({
-              url: `../arcall/index/index`
-            })
-          }
-        }
-      });
-
-
+      wx.navigateTo({
+        url: `../arcall/index/index`
+      })
     } else if (type == 0) {
       wx.navigateTo({
         url: `../meet/index/index`
