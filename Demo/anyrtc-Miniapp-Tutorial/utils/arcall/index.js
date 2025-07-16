9db60bc4ce0f5ef884538652d8b452d88ee8dfcd
chudong <604748948@qq.com>
2025-07-16 17:15:35 +0800

    refactor: 删除小程序 ArCall 相关代码，部分代码影响会议重连效果

--- Demo/anyrtc-Miniapp-Tutorial/utils/arcall/index.js
+++
@@ -1,5 +0,0 @@
-export const Until = require("./util");
-export const Store = require("./store");
-export const RTC = require("./rtc");
-export const RTM = require("./rtm");
-export const liveCode = require("./live-code");
